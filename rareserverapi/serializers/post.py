b186722bba19620d57129fb331469ffb3ee64e6e
Jonathan Shearon <jonathan.shearon@gmail.com>
2020-12-10 20:59:04 -0600

    updated serializer for depth = 1

--- rareserverapi/serializers/post.py
+++ rareserverapi/serializers/post.py
@@ -4,4 +4,7 @@ from rareserverapi.models import Post
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
+        rareuser = serializers.PrimaryKeyRelatedField(many=False, read_only=True)
+        category = serializers.PrimaryKeyRelatedField(many=False, read_only=True)
         fields = ('id', 'rareuser', 'category', 'title', 'publication_date', 'image_url', 'content', 'approved')
+        depth = 1
