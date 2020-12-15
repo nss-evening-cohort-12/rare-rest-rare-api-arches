bae450069b9124ed6d6630da64943e7ae470267b
Phonesalo Sihanorak <psihanorak@gmail.com>
2020-12-14 19:04:59 -0600

    Full CRUD on Categories

--- rareserverapi/views/categories.py
+++ rareserverapi/views/categories.py
@@ -20,10 +20,50 @@ class CategoriesViewSet(ViewSet):
         except Exception as ex:
           return HttpResponseServerError(ex)
 
+
     def list(self, request):
 
-      categories = Category.objects.all().order_by('label')
+        categories = Category.objects.all().order_by("label")
 
-      serializer = CategorySerializer(
-        categories, many=True, context={'request': request})
-      return Response(serializer.data)
+        serializer = CategorySerializer(
+          categories, many=True, context={'request': request})
+        return Response(serializer.data)
+
+    
+    def create(self, request):
+
+        category = Category()
+        category.label = request.data["label"]
+
+        try:
+          category.save()
+          serializer = CategorySerializer(category, context={'request': request})
+          return Response(serializer.data)
+
+        except ValidationError as ex:
+          return Response({'reason': ex.message}, status=status.HTTP_400_BAD_REQUEST)
+
+
+    def destroy(self, request, pk=None):
+
+        try:
+          category = Category.objects.get(pk=pk)
+          category.delete()
+
+          return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+        except Category.DoesNotExist as ex:
+          return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+        except Exception as ex:
+          return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+    def update(self, request, pk=None):
+
+        category = Category.objects.get(pk=pk)
+        category.label = request.data["label"]
+
+        category.save()
+
+        return Response({}, status=status.HTTP_204_NO_CONTENT)
