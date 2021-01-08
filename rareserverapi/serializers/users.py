455f12eefd1a2053edd9ec4f8b525ae30733a068
Jonathan Shearon <jonathan.shearon@gmail.com>
2021-01-07 18:53:13 -0600

    can now update all fields in user and  rareuser

--- rareserverapi/serializers/users.py
+++ rareserverapi/serializers/users.py
@@ -1,10 +1,12 @@
 from rest_framework import serializers
 from rareserverapi.models import RareUsers
+from django.contrib.auth import get_user_model
 
 
 class UsersSerializer(serializers.ModelSerializer):
     class Meta:
         model = RareUsers
+        user = get_user_model()
         posts = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
         fields = ('id', 'user', 'bio', 'profile_image_url',
                   'created_on', 'active', 'posts')
