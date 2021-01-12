eb8dea89f606e4fdbd28eee535e9d8e11cb70b3f
Nick Walters <nswalters.s@gmail.com>
2021-01-11 19:06:19 -0600

    Add reactions API endpoint

--- rareserverapi/serializers/__init__.py
+++ rareserverapi/serializers/__init__.py
@@ -3,3 +3,4 @@ from .category import CategorySerializer
 from .comment import CommentSerializer
 from .users import UsersSerializer
 from .tag import TagSerializer
+from .reaction import ReactionSerializer
--- rareserverapi/serializers/post.py
+++ rareserverapi/serializers/post.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from rareserverapi.models import Post, PostReactions
+from rareserverapi.models import Post
 
 
 class PostSerializer(serializers.ModelSerializer):
