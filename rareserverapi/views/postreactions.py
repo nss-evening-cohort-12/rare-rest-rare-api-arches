c503a647ad21add4f2f81b948697dd6a9b755495
Nick Walters <nswalters.s@gmail.com>
2021-01-12 22:00:42 -0600

    Deleting reactions from a post now works

--- rareserverapi/views/postreactions.py
+++ rareserverapi/views/postreactions.py
@@ -5,21 +5,47 @@ from django.contrib.auth.models import User
 from rest_framework import serializers, status
 from rest_framework.response import Response
 from rest_framework.viewsets import ViewSet
+from rest_framework.decorators import action
 
 
 class PostReactionViewSet(ViewSet):
     # Data that should be received in a request:
     # {
     #     "post_id": 0,  -> id of post
-    #     "reaction_id": 0, -> id of reaction
-    #     "user_id": 0 -> id of the user (not RareUser)
+    #     "reaction_id": 0 -> id of reaction
     # }
+    #
+    # Note: The 'user_id' is pull from the request details
+
+    # Because we aren't deleting a specific postreaction record directly, we need to use our own function so we don't have to pass in a pk via the path since deletion data will be coming in via the body.
+    #
+    # SEE: https://stackoverflow.com/a/51637525/13029557 for more details
+    @action(methods=['DELETE'], detail=False)
+    def delete(self, request):
+        user_id = request.user.id
+        try:
+            post = Post.objects.get(pk=request.data["post_id"])
+            postreaction = PostReactions.objects.get(
+                user_id=user_id, reaction_id=request.data["reaction_id"])
+
+            post.reactions.remove(postreaction)
+
+            # user = User.objects.get(pk=request.data["user_id"])
+            # reaction = Reaction.objects.get(pk=request.data["reaction_id"])
+
+        except Exception as ex:
+            return HttpResponseServerError(ex)
+
+        return Response({}, status=status.HTTP_204_NO_CONTENT)
 
     def create(self, request):
+        # Python provides the user data that matches the token used in the request. We use this to grab the id for that user's record
+        user_id = request.user.id
+
         # Need to make sure the data that was provided was valid, otherwise, handle gracefully (i.e. make sure the post, user, and reaction all exist before we attempt to use them)
         try:
             post = Post.objects.get(pk=request.data["post_id"])
-            user = User.objects.get(pk=request.data["user_id"])
+            user = User.objects.get(pk=user_id)
             reaction = Reaction.objects.get(pk=request.data["reaction_id"])
 
         except Exception as ex:
@@ -28,7 +54,7 @@ class PostReactionViewSet(ViewSet):
         try:
             # If the specific reaction and user combination already exists (i.e. that user has used that reaction before), grab that combo
             postreaction = PostReactions.objects.get(
-                user_id=request.data["user_id"], reaction_id=request.data["reaction_id"])
+                user_id=user_id, reaction_id=request.data["reaction_id"])
 
             # if postreaction does exist, we then attach that reaction to the post, returning the postreaction data that was created (the user and the reaction combo that was added)
             post.reactions.add(postreaction)
